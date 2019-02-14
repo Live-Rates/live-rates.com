0fcd3ffb302b1714fc9ab671a7588a6d891ae5fd
acegilz <weirkved@netcabo.pt>
2019-02-14 02:47:42 +0000

    Trial Streaming

--- examples/streaming_client.js
+++ examples/streaming_client.js
@@ -1,7 +1,12 @@
 const io = require('socket.io-client');
 const socket = io('https://wss.live-rates.com/')
 
-var key = 'XXXXXXX' //YOUR LIVE-RATES SUBSCRIPTION KEY
+//var key = 'XXXXXXX' //YOUR LIVE-RATES SUBSCRIPTION KEY
+
+//Use the 'trial' as key to establish a 2-minute streaming connection with real-time data. 
+//After the 2-minute test, the server will drop the connection and block the IP for an Hour.
+var key = 'trial' 
+
 
 socket.on('connect', function(){
 	socket.emit('key', key, (data) => {
