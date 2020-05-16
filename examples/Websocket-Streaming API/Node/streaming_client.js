26e0fc6e93468bc28b191c4f420d61f30401bc2b
acegilz <weirkved@netcabo.pt>
2020-05-16 15:48:23 +0100

    socket.emit('instruments', instruments);

--- examples/Websocket-Streaming API/Node/streaming_client.js
+++ examples/Websocket-Streaming API/Node/streaming_client.js
@@ -1,36 +1,32 @@
 const io = require('socket.io-client');
 const socket = io('https://wss.live-rates.com/')
 
-//var key = 'XXXXXXX' //YOUR LIVE-RATES SUBSCRIPTION KEY
-
-// if you want to subscribe only specific instruments, add them to the following object, otherwise leave this commented
-// var instruments = ['EURUSD', 'USDJPY', 'BTCUSD', 'ETH']
+var instruments = ['EURUSD', 'USDJPY', 'BTCUSD', 'ETH']
 
 //Use the 'trial' as key to establish a 2-minute streaming connection with real-time data.
 //After the 2-minute test, the server will drop the connection and block the IP for an Hour.
+
 var key = 'trial' 
+//var key = 'XXXXXXX' //YOUR LIVE-RATES SUBSCRIPTION KEY
 
 
 socket.on('connect', function() {
-  socket.emit('key', key, (data) => {
-    console.log(data); //RESPONSE FROM SERVER
-  });
+  socket.emit('key', key);
+  
+  // if you want to subscribe only specific instruments, emit instruments. To receive all instruments, comment the line below.
+  socket.emit('instruments', instruments);
+  
 });
 
 socket.on('rates', function(msg) {
+
+  //Do what you want with the Incoming Rates... Enjoy!
   try {
     let obj = JSON.parse(msg);
-
-    // Subscribe only specific rates
-    if (typeof instruments === 'undefined' || instruments.length == 0) {
-      console.log(obj)
-    } else {
-      if (instruments.includes(obj.currency)) {
-        console.log(obj)
-      }
-    }
+    console.log(obj)
 
   } catch (e) {
     console.log(msg)
   }
+
 });
