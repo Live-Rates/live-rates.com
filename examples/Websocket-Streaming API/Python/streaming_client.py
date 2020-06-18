2faa2f10297aafc9bc492f863a26984cf13f3d0a
acegilz <weirkved@netcabo.pt>
2020-06-18 10:12:56 +0100

    Update streaming_client.py

--- examples/Websocket-Streaming API/Python/streaming_client.py
+++ examples/Websocket-Streaming API/Python/streaming_client.py
@@ -9,14 +9,16 @@ sio = socketio.Client(ssl_verify=False)
 
 @sio.event
 def connect():
-	#Use the 'trial' as key to establish a 2-minute streaming connection with real-time data.
-	#After the 2-minute test, the server will drop the connection and block the IP for an Hour.
-	sio.emit('key', 'trial')
 	
 	## USE THIS LIST TO FILTER AND RECEIVE ONLY INSTRUMENTS YOU NEED. LEAVE EMPTY TO RECEIVE ALL
 	#if you want to subscribe only specific instruments, emit instruments. To receive all instruments, comment the lines below.
 	instruments = ['EURUSD', 'USDJPY', 'BTCUSD', 'ETH']
 	sio.emit('instruments', instruments);
+	
+	
+	#Use the 'trial' as key to establish a 2-minute streaming connection with real-time data.
+	#After the 2-minute test, the server will drop the connection and block the IP for an Hour.
+	sio.emit('key', 'trial')
 
 @sio.event
 def rates(rates):
