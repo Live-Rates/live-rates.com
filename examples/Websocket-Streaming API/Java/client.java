eafc59bfd78243758a8ef90d53314b0f6b1f1357
a_coder_online <weirkved@netcabo.pt>
2021-04-08 13:40:58 +0100

    Create client.java

---
+++ examples/Websocket-Streaming API/Java/client.java
@@ -0,0 +1,30 @@
+// Install Socket.io Java Client
+// https://socketio.github.io/socket.io-client-java/installation.html
+
+Socket socket = IO.socket("https://wss3.live-rates.com");
+
+String key = 'trial' 
+//String key = 'XXXXXXX' // YOUR LIVE-RATES SUBSCRIPTION KEY
+  
+socket.on("connect", new Emitter.Listener() {
+    @Override
+    public void call(Object... args) {
+        // if you want to subscribe only specific instruments, emit instruments. To receive all instruments, comment the line below.
+        String[] instruments = ['EURUSD', 'USDJPY', 'BTCUSD', 'ETH']
+        socket.emit('instruments', instruments);
+      
+        socket.emit("key", key);
+    }
+});
+
+
+socket.on("rates", function(msg) {
+
+  //Do what you want with the Incoming Rates... Enjoy!
+  try {
+    System.out.println(msg);
+  } catch (e) {
+    System.out.println(e);
+  }
+
+});
